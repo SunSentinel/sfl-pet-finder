bb53a6c03d1931762e9c1399a06fbf5d9328f567
Aric Chokey <achokey21@gmail.com>
2021-04-07 12:42:27 -0400

    Add data fetch

---
+++ README.md
@@ -0,0 +1,6 @@
+# South Florida Pet Finder 
+
+A Serverless function that pulls pet intake data from Broward County Animal Care.
+
+- See the county's dashboard at https://experience.arcgis.com/template/355ece96749b47b7bf0e4967e8f22fee
+- See the resulting data file at https://s3.amazonaws.com/content.sun-sentinel.com/data/pet-finder/broward_pets.csv
--- handler.py
+++ handler.py
@@ -1,24 +1,51 @@
+import io
+import csv
 import json
+from datetime import datetime
+
+import boto3
+import botocore
+from esridump.dumper import EsriDumper
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def main(event, context):
+    d = EsriDumper("https://services.arcgis.com/JMAJrTsHNLrSsWf5/arcgis/rest/services/Animal_Care_Intake_and_Outcome_Data/FeatureServer/0")
+    pet_list = []
+    for feature in d:
+        if feature["properties"]["OutcomeWeightDate"] > 0:
+            feature["properties"]["OutcomeWeightDate"] = to_datetime(feature["properties"]["OutcomeWeightDate"])
+            feature["properties"]["Extra11"] = to_datetime(feature["properties"]["Extra11"])
+            feature["properties"]["LatestUpdate"] = to_datetime(feature["properties"]["LatestUpdate"])
+            feature["properties"]["IntakeWeightDate"] = to_datetime(feature["properties"]["IntakeWeightDate"])
+            feature["properties"]["IntakeDate"] = to_datetime(feature["properties"]["IntakeDate"])
+            feature["properties"]["OutcomeDate"] = to_datetime(feature["properties"]["OutcomeDate"])
+            feature["properties"]["WeightDate"] = to_datetime(feature["properties"]["WeightDate"])
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+            pet_list.append(feature["properties"])
+    
+    save_S3_sheet(pet_list, "broward_pets.csv")
+    
 
-    return response
+def save_S3_sheet(input_data, filename):
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    AWS_BUCKET_NAME = 'content.sun-sentinel.com'
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(AWS_BUCKET_NAME)
+    path = "data/pet-finder/" + filename
+    
+    with io.StringIO() as csvfile:
+        headers = set(list(input_data[0].keys()) + list(input_data[-1].keys()))
+        csv_writer = csv.DictWriter(csvfile, fieldnames=headers)
+        csv_writer.writeheader()
+        for row in input_data:
+            csv_writer.writerow(row)
+
+        data = csvfile.getvalue()
+
+        bucket.put_object(Key=path, Body=data)
+
+
+
+
+def to_datetime(dt):
+    return datetime.fromtimestamp(dt/1000)
